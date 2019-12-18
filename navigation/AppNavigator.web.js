b09a8078fc7922e25a5b6e8857beaacfea36f22d
Faruk Kaledibi <fkaledibi@rbbn.com>
2019-12-18 16:52:16 +0300

    Web navigation updated

--- navigation/AppNavigator.web.js
+++ navigation/AppNavigator.web.js
@@ -1,13 +1,55 @@
+import React from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
 import { createBrowserApp } from '@react-navigation/web';
-import { createSwitchNavigator } from 'react-navigation';
+// import { createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import Home from '../screens/Home';
+import Item from '../screens/Item';
 
-import AppNavigator from './AppNavigator';
+const Navigator = createStackNavigator(
+  {
+    Home: {
+      screen: Home,
+      path: '/',
+      navigationOptions: () => ({
+        headerStyle: {
+          backgroundColor: '#0477BF',
+        },
+        headerLeft: () => (
+          <Text style={{ fontSize: 17, color: 'white', fontWeight: 'bold', marginLeft: 16, }}>Agenda</Text>
+        ),
+        headerRight: () => (
+          <TouchableOpacity>
+            <Ionicons
+              name="ios-calendar"
+              size={26}
+              style={{ marginRight: 18 }}
+              color="white"
+            />
+          </TouchableOpacity>
+        ),
+        headerTintColor: '#fff',
+        headerBackTitle: 'Agenda',
+      }),
+    },
+    Item: {
+      screen: Item,
+      path: '/',
+      navigationOptions: () => ({
+        headerStyle: {
+          backgroundColor: '#0477BF',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }),
+    },
+  }
+);
 
-const switchNavigator = createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: AppNavigator,
-});
-switchNavigator.path = '';
+createStackNavigator.path = '';
 
-export default createBrowserApp(AppNavigator, { history: 'hash' });
+export default createBrowserApp(Navigator, { history: 'hash' });
